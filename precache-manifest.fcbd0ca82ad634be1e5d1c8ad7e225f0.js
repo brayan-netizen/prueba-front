c4db1453acb43337d3c507669f364bbd5641dc6d
Brayan Personal <brayanocampo45@gmail.com>
2023-07-10 20:00:45 -0500

    Updates

---
+++ precache-manifest.fcbd0ca82ad634be1e5d1c8ad7e225f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fcf3dec7763ece77ab9",
+    "url": "/brayan-netizen/prueba-front.git/static/css/main.4cf39860.chunk.css"
+  },
+  {
+    "revision": "8fcf3dec7763ece77ab9",
+    "url": "/brayan-netizen/prueba-front.git/static/js/main.485c8b23.chunk.js"
+  },
+  {
+    "revision": "eb1576f455fda2357255",
+    "url": "/brayan-netizen/prueba-front.git/static/js/runtime~main.7654c3a9.js"
+  },
+  {
+    "revision": "c75f34afe46fb59ff237",
+    "url": "/brayan-netizen/prueba-front.git/static/css/2.2971ed1d.chunk.css"
+  },
+  {
+    "revision": "c75f34afe46fb59ff237",
+    "url": "/brayan-netizen/prueba-front.git/static/js/2.0b728f32.chunk.js"
+  },
+  {
+    "revision": "8824d8dc60117fb8fcab",
+    "url": "/brayan-netizen/prueba-front.git/static/js/3.b99af0ea.chunk.js"
+  },
+  {
+    "revision": "6095b94ff40bfcb06a4c49099041d55b",
+    "url": "/brayan-netizen/prueba-front.git/index.html"
+  }
+];
